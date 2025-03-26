2948d8323e76c649ee063ffe01b4d2c028e7343e
Gary Caen <garycaen@gmail.com>
2025-03-26 22:07:38 +0000

    starting to code, its shit

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 print("This is my task app")
+
+
+#CLI using argparse - try with sys lib
+
+#flags to add task, remove, update, show and finish task
+# e.g doomtask add "cook meal for wife's boyfriend" / doomtask update 1 -in_progress / doomtask remove 1 / doomtask show
+# saved in JSON Dict - figure out how json works
+
+import sys
+
+if len(sys.argv) > 4:
+    print("Too many arguments")
+elif len(sys.argv) == 1:
+    print("Too few arguments")
+else:
+    if sys.argv[1] == "add":
+        #add(sys.argv[2])
+        pass
+    elif sys.argv[1] == "remove":
+        #remove(sys.argv[2])
+        pass
+    elif sys.argv[1] == "show":
+        #show()
+        pass
+    elif sys.argv[1] == "finish":
+        #finish(sys.argv[2])
+        pass
+    else:
+        print("command not reconised")
+        
